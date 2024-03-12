a692631f377ae8c1481d5220924efc1dfb2ab962
Kirusnabavani <user.kirush092@gmail.com>
2024-03-12 23:21:31 +0530

    js code

---
+++ index.js
@@ -0,0 +1,26 @@
+
+var calculate = document.getElementById('calculate');
+var calculateAge = document.getElementById('calculateAge');
+
+calculate.addEventListener('click', function(){
+    
+    var birthDate = new Date(document.getElementById('birth_date').value);
+    var todayDate = new Date();
+    
+    var ageYear = todayDate.getFullYear() - birthDate.getFullYear();
+    var ageMonth = todayDate.getMonth() - birthDate.getMonth();
+    var ageDay = todayDate.getDate() - birthDate.getDate();
+
+    if (ageMonth < 0 || (ageMonth === 0 && todayDate.getDate() < birthDate.getDate())) {
+        ageYear--;
+        ageMonth += 12;
+    }
+
+    if (ageDay < 0) {
+        var lastMonth = new Date(todayDate.getFullYear(), todayDate.getMonth() - 1, 0);
+        ageDay += lastMonth.getDate();
+        ageMonth--;
+    }
+
+    calculateAge.innerHTML = "Your Age: " + ageYear + " Years, " + ageMonth + " Months, " + ageDay + " Days";
+});
